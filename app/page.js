5f6b73198490bf68cea153924d75880fd3993589
Muhammad Mehtab Arfa <muhammadmehtabarfa@gmail.com>
2024-05-22 21:09:40 +0500

    added content

--- app/page.js
+++ app/page.js
@@ -1,6 +1,3 @@
-import Navbar from "@/components/shared/Navbar";
-import Image from "next/image";
-import { Children } from "react";
 import Whitepaper from "@/components/whitepaper/Whitepaper";
 export default function Home() {
   return <><Whitepaper/></>;
